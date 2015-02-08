afcec770296198afa14e338a2502f7c205d9def8
zhaotr <zhaotr@gmail.com>
2015-02-08 11:29:49 +0800

    add to source control

--- MiniCalculator/ViewController.swift
+++ MiniCalculator/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    var operand1 = ""
+    var operand2 = ""
+    var oper = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didClicked(sender: UIButton) {
+        var labelText = sender.titleLabel?.text
+
+        switch labelText! {
+        case "+","-","x","/" :
+            self.oper = labelText!
+        case "=":
+            var oper1 = self.operand1.toInt();
+            var oper2 = self.operand2.toInt()
+            var result = ""
+            switch self.oper {
+            case "+" :
+                result = "\(oper1!+oper2!)"
+            case "-" :
+                result = "\(oper1!-oper2!)"
+            case "x" :
+                result = "\(oper1!*oper2!)"
+            case "/" :
+                result = "\(oper1!/oper2!)"
+            default:
+                println("error")
+            }
+            self.resultLabel.text = result
+            self.operand1 = result
+            self.operand2 = ""
+            self.oper = ""
+        case "C" :
+            self.resultLabel.text = "0"
+            self.operand1 = ""
+            self.operand2 = ""
+            self.oper = ""
+        default:
+            if self.oper == "" {
+                self.operand1 += labelText!
+                self.resultLabel.text = operand1
+            } else {
+                self.operand2 += labelText!
+                self.resultLabel.text = operand2
+            }
+        }
+
+    }
 
 }
 
